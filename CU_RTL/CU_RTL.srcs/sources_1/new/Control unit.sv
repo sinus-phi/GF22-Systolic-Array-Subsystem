cb3f809fd31f7ded289047f78cf647ab4d23275c
Pradeepthi <go93fap@tum.de>
2026-05-08 08:30:19 +0200

    Added CU_RTL Vivado project

---
+++ CU_RTL/CU_RTL.srcs/sources_1/new/Control unit.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07.05.2026 20:34:20
+// Design Name: 
+// Module Name: Control unit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Control unit(
+
+    );
+endmodule
